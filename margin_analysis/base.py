0835b842a4fa1d524bc49f2fcc1f1949ba66befe
usugamiSou <usugami@anzupop.com>
2025-09-30 16:50:59 +0800

    ♻️ refactor (base): restructure Exchange and Variety classes   - convert Exchange and PositionType from enums to string-based classes   - reorganize variety data into exchange-specific sets for better organization   - update exchange code mapping with improved error handling and naming   - refactor commodity pair checking to use frozensets and exchange context

--- margin_analysis/base.py
+++ margin_analysis/base.py
@@ -1,119 +1,94 @@
-from enum import Enum, unique, auto
+class Exchange:
+    CFFEX = 'CFFEX'
+    SSE = 'SSE'
+    SZSE = 'SZSE'
+    SHFE = 'SHFE'
+    CZCE = 'CZCE'
+    DCE = 'DCE'
+    GFEX = 'GFEX'
 
+    EquityExchanges = {CFFEX, SSE, SZSE}
+    CommodityExchanges = {SHFE, DCE, CZCE, GFEX}
 
-@unique
-class Exchange(Enum):
-    CFE = auto()
-    SH = auto()
-    SZ = auto()
-    SHFE = auto()
-    DCE = auto()
-    CZCE = auto()
-    GFEX = auto()
-
-    @classmethod
-    def from_string(cls, string):
-        match string:
-            case 'CCFX' | 'CFE':
-                return Exchange.CFE
-            case 'XSHG' | 'SH':
-                return Exchange.SH
-            case 'XSHE' | 'SZ':
-                return Exchange.SZ
-            case 'XSGE' | 'SHFE':
+    @staticmethod
+    def from_code(exchange_code: str) -> str:
+        exchange_code = exchange_code.upper()
+        match exchange_code:
+            case 'CFE' | 'CCFX' | 'CFFEX':
+                return Exchange.CFFEX
+            case 'SH' | 'XSHG':
+                return Exchange.SSE
+            case 'SZ' | 'XSHE':
+                return Exchange.SZSE
+            case 'SHFE' | 'XSGE':
                 return Exchange.SHFE
-            case 'XZCE' | 'CZCE':
+            case 'DZCE' | 'XZCE':
                 return Exchange.CZCE
-            case 'XDCE' | 'DCE':
+            case 'DCE' |'XDCE':
                 return Exchange.DCE
             case 'GFEX':
                 return Exchange.GFEX
+            case _:
+                raise ValueError(f'Invalid exchange code: {exchange_code}.')
 
 
-@unique
-class PositionType(Enum):
-    Future = auto()
-    Option = auto()
-    Stock = auto()
-
-@unique
-class FutureVariety(Enum):
-    # 中国金融期货交易所
-    IF = 'IF'
-    IC = 'IC'
-    IM = 'IM'
-    IH = 'IH'
-
-    # 上海期货交易所
-    # TODO
-
-    # 大连商品交易所
-    A = 'A'    # 黄大豆1号
-    B = 'B'    # 黄大豆2号
-    M = 'M'    # 豆粕
-    Y = 'Y'    # 豆油
-    P = 'P'    # 棕榈油
-    C = 'C'     # 玉米
-    CS = 'CS'    # 玉米淀粉
-    RR = 'RR'    # 粳米
-    JD = 'JD'    # 鸡蛋
-    LH = 'LH'    # 生猪
-    FB = 'FB'    # 纤维板
-    BB = 'BB'    # 胶合板
-    LG = 'LG'    # 玻璃
-    JM = 'JM'    # 焦煤
-    J = 'J'    # 焦炭
-    I = 'I'    # 铁矿石
-    L = 'L'    # 聚乙烯
-    V = 'V'    # 聚氯乙烯
-    PP = 'PP'    # 聚丙烯
-    EG = 'EG'    # 乙二醇
-    EB = 'EB'    # 苯乙烯
-    PG = 'PG'    # 液化石油气
-    BZ = 'BZ'    # 纯苯
-
-    # 郑州商品交易所
-
-    # TODO
-
-    # 广州期货交易所
-    # TODO
-
-    # 上海证券交易所、深圳证券交易所
-    ETF = 'ETF'    # 期权标的, 非期货
-    
-    @classmethod
-    def is_commodity_pair(cls, variety1, variety2):
-        return ((variety1, variety2) in cls._commodity_pairs or
-                (variety2, variety1) in cls._commodity_pairs)
+class PositionType:
+    Future = 'Future'
+    Option = 'Option'
+    Stock = 'Stock'
 
 
-FutureVariety._commodity_pairs = (
-        # 大连商品交易所
-        (FutureVariety.A, FutureVariety.B),
-        (FutureVariety.A, FutureVariety.M),
-        (FutureVariety.B, FutureVariety.M),
-        (FutureVariety.Y, FutureVariety.P),
-        (FutureVariety.C, FutureVariety.CS),
-        (FutureVariety.JM, FutureVariety.J),
-        (FutureVariety.JM, FutureVariety.I),
-        (FutureVariety.J, FutureVariety.I),
-        (FutureVariety.L, FutureVariety.V),
-        (FutureVariety.L, FutureVariety.PP),
-        (FutureVariety.L, FutureVariety.EG),
-        (FutureVariety.L, FutureVariety.EB),
-        (FutureVariety.L, FutureVariety.PG),
-        (FutureVariety.V, FutureVariety.PP),
-        (FutureVariety.V, FutureVariety.EG),
-        (FutureVariety.V, FutureVariety.EB),
-        (FutureVariety.V, FutureVariety.PG),
-        (FutureVariety.PP, FutureVariety.EG),
-        (FutureVariety.PP, FutureVariety.EB),
-        (FutureVariety.PP, FutureVariety.PG),
-        (FutureVariety.EG, FutureVariety.EB),
-        (FutureVariety.EG, FutureVariety.PG),
-        (FutureVariety.EB, FutureVariety.PG),
+class Variety:
+    EquityVarieties = {
+        'CFFEX': {
+            'IF', 'IC', 'IM', 'IH',
+            'IO', 'MO', 'HO',
+        },
+        'SSE': {
+            '510050', '510300', '510500', '588000', '588080',
+        },
+        'SZSE': {
+            '159901', '159915', '159919', '159922',
+        },
+    }
 
-        # 郑州商品交易所
-        # TODO
-    )
+    CommodityVarieties = {
+        'SHFE': {
+            'CU', 'BC', 'AL', 'ZN', 'PB', 'NI', 'SN', 'AO', 'AD',
+            'AU', 'AG', 'RB', 'WR', 'HC', 'SS', 'SC', 'LU', 'FU',
+            'BU', 'BR', 'RU', 'NR', 'SP', 'OP', 'EC',
+        },
+        'DCE': {
+            'A', 'B', 'M', 'Y', 'P', 'C', 'CS', 'RR', 'JD', 'LH',
+            'FB', 'BB', 'LG', 'JM', 'J', 'I', 'L', 'V', 'PP',
+            'EG', 'EB', 'PG', 'BZ',
+        },
+        'CZCE': set(),
+        'GFEX': {
+            'PS', 'LC', 'SI',
+        },
+    }
+
+    @staticmethod
+    def is_commodity_pair(variety1: str, variety2: str, exchange: str) -> bool:
+        if (
+            exchange not in Variety.CommodityPairs or
+            variety1 not in Variety.CommodityVarieties[exchange] or
+            variety2 not in Variety.CommodityVarieties[exchange]
+        ):
+            return False
+        return frozenset((variety1, variety2)) in Variety.CommodityPairs[exchange]
+
+    CommodityPairs = {
+        'CZCE': set(),
+        'DCE': {
+            frozenset(('A', 'B')), frozenset(('A', 'M')), frozenset(('B', 'M')),
+            frozenset(('Y', 'P')), frozenset(('C', 'CS')), frozenset(('JM', 'J')),
+            frozenset(('JM', 'I')), frozenset(('J', 'I')), frozenset(('L', 'V')),
+            frozenset(('L', 'PP')), frozenset(('L', 'EG')), frozenset(('L', 'EB')),
+            frozenset(('L', 'PG')), frozenset(('V', 'PP')), frozenset(('V', 'EG')),
+            frozenset(('V', 'EB')), frozenset(('V', 'PG')), frozenset(('PP', 'EG')),
+            frozenset(('PP', 'EB')), frozenset(('PP', 'PG')), frozenset(('EG', 'EB')),
+            frozenset(('EG', 'PG')), frozenset(('EB', 'PG')),
+        },
+    }
