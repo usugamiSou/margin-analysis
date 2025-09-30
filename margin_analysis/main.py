67e2256679bafd49dcc31809ef34ce653d1a55b3
usugamiSou <usugami21@gmail.com>
2025-09-30 17:15:22 +0800

    ♻️ refactor (main): restructure main script

--- margin_analysis/main.py
+++ margin_analysis/main.py
@@ -1,18 +1,34 @@
-import pandas as pd
-from holding_data_processor import HoldingDataProcessor
+import os
+from data import DataLoader, HoldingDataProcessor
 from margin_optimizer import MarginOptimizer
 
 
-if __name__ == '__main__':
-    holding = pd.read_excel('kdb_pos.xlsx').dropna()
-    future_data = pd.read_csv('future_quote.csv', encoding='GB2312').dropna()
-    option_data = pd.read_csv('option_quote.csv', encoding='GB2312').dropna()
-    option_data2 = pd.read_csv('option_quote2.csv', encoding='GB2312').dropna()
-    holding = HoldingDataProcessor.preprocess_holding(holding, future_data, [option_data, option_data2])
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+data_path = os.path.join(root_path, 'data')
+input_path = os.path.join(data_path, 'input')
+output_path = os.path.join(data_path, 'output')
+temp_path = os.path.join(data_path, 'temp')
+
+
+def main():
+    holding = DataLoader.load_holding(os.path.join(input_path, 'kdb_pos.xlsx'))
+    future_data = DataLoader.load_market_data(os.path.join(input_path, 'future_quote.csv'))
+    option_data = DataLoader.load_market_data(os.path.join(input_path, 'option_quote.csv'))
+    margin_ratio, _, _ = DataLoader.load_params(os.path.join(input_path, 'marginCfg.xlsx'))
+    data_processor = HoldingDataProcessor(holding, future_data, option_data,
+                                          None, None, margin_ratio)
+    holding = data_processor.process()
+    holding.to_csv(os.path.join(temp_path, 'processed_holding.csv'),
+                   index=False, encoding='GB2312')
+
     optimizer = MarginOptimizer(holding, is_close=False)
     optimization_result = optimizer.run()
     print('Optimization Completed.')
+    output_filename = 'optimal_holding.csv'
+    optimization_result.to_csv(os.path.join(output_path, output_filename),
+                               index=False, encoding='GB2312')
+    print(f'The optimal holding has been saved to {os.path.join(output_path, output_filename)}.')
 
-    output_path = 'optimal_holding.csv'
-    optimization_result.to_csv(output_path, index=False, encoding='GB2312')
-    print(f'The optimal holding is saved to {output_path}.')
+
+if __name__ == '__main__':
+    main()
