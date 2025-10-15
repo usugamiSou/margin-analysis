31a3abebd3ad2b9ec6b34a962553d0ceb7a19741
usugamiSou <usugami@anzupop.com>
2025-10-15 16:26:14 +0800

    ✨ feat (margin-utils): add vectorized margin calculation methods

--- margin_analysis/margin_utils.py
+++ margin_analysis/margin_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from base import PositionType, Exchange
 
@@ -74,3 +75,31 @@ class MarginCalculator:
             udl_margin = self.udl_price * self.margin_ratio
             return self.multiplier * (
                 self.close_price + udl_margin - 0.5 * min(otm, udl_margin))
+
+    def calc_future_vec(self, close_price_vec: np.ndarray) -> np.ndarray:
+        """
+        给定期货价格情形, 计算期货持仓保证金 (支持向量化计算)
+
+        Args:
+            close_price (ndarray): 期货价格情形, shape: (*scenarios_dim)
+
+        Returns:
+            ndarray: 各情形下的保证金金额, shape: (*scenarios_dim)
+        """
+        calc_vec = np.vectorize(lambda p: self.calc_future(close_price=p))
+        return calc_vec(close_price_vec)
+
+    def calc_option_vec(self, udl_price_vec: np.ndarray,
+                        close_price_vec: np.ndarray) -> np.ndarray:
+        """
+        给定标的价格与期权价格情形, 计算期权持仓保证金 (支持向量化计算)
+
+        Args:
+            udl_price_vec (ndarray): 标的价格情形, shape: (*scenarios_dim)
+            close_price_vec (ndarray): 期权价格情形, shape: (*scenarios_dim)
+
+        Returns:
+            ndarray: 各情形下的保证金金额, shape: (*scenarios_dim)
+        """
+        calc_vec = np.vectorize(lambda s, p: self.calc_option(udl_price=s, close_price=p))
+        return calc_vec(udl_price_vec, close_price_vec)
